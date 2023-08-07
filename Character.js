8e3560d43a786e4e89e56dabd8d4179c0c11534b
jennykgb <jennykgb@gmail.com>
2023-08-07 16:20:50 -0400

    Initial game app

---
+++ Character.js
@@ -0,0 +1,28 @@
+import {getDiceRollArray} from "./utils.js"
+
+function Character(data){
+
+    Object.assign(this, data)
+
+    this.getCharacterHtml = function(){
+        const {elementId, name, avatar, health, diceCount} = this
+        const diceHtml = this.getDiceHtml(diceCount)
+        return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}"/>
+                <p class="health">health: <b> ${health} </b></p>
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`
+    }
+
+    this.getDiceHtml = function(diceCount){
+        return getDiceRollArray(diceCount).map(function(roll){
+            return `<div class="dice">${roll}</div>`
+        }).join(" ")
+    }
+}
+
+export  default Character
---
+++ index.js
@@ -0,0 +1,13 @@
+import characterData from "./data.js"
+import Character from "./Character.js"
+
+
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+
+function render(){
+    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
+    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
+}
+
+render()
